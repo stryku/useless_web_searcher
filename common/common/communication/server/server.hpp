dd24e809ccc9b7e0db71331f1d76aee3899c607d
stryku <stryku2393@gmail.com>
2018-02-18 00:02:57 +0100

    added set_message_handler() method

--- common/common/communication/server/server.hpp
+++ common/common/communication/server/server.hpp
@@ -3,6 +3,7 @@
 #include <zmq.hpp>
 
 #include <string>
+#include <memory>
 
 namespace usl::common::communication::server
 {
@@ -13,7 +14,38 @@ namespace usl::common::communication::server
 
         void bind(std::string_view bind_address);
 
+        template <typename T>
+        void set_message_handler(T message_handler)
+        {
+            using message_handler_t = message_handler_impl<T>;
+            m_message_handler = std::make_unique<message_handler_t>(std::move(message_handler));
+        }
+
     private:
+        class base_message_handler
+        {
+        public:
+            virtual void handle(const std::string& message) = 0;
+        };
+
+        template <typename T>
+        class message_handler_impl : public base_message_handler
+        {
+        public:
+            message_handler_impl(T message_handler)
+                : m_message_handler{ std::move(message_handler) }
+            {}
+
+            void handle(const std::string& message) override
+            {
+                m_message_handler.handle(message);
+            }
+
+        private:
+            T m_message_handler;
+        };
+
         zmq::socket_t m_socket;
+        std::unique_ptr<base_message_handler> m_message_handler;
     };
 }
