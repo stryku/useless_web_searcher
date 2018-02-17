0fee72fb064a7e7493b39a07e4835687aa18eb95
stryku <stryku2393@gmail.com>
2018-02-17 21:24:30 +0100

    introduce Crawler class

--- engine/crawler/crawler.py
+++ engine/crawler/crawler.py
@@ -1,4 +1,26 @@
 import urllib.request
-site = urllib.request.urlopen("https://stackoverflow.com").read()
+import sys
 
-print(site)
+
+class Crawler:
+    def __init__(self, working_dir, crawl_frontier_ip):
+        self.working_dir = working_dir
+        self.crawl_frontier_ip = crawl_frontier_ip
+
+    def run(self):
+        while True:
+            site = urllib.request.urlopen("https://stackoverflow.com")
+            print(site.headers)
+
+
+def main():
+    while True:
+        try:
+            crawler = Crawler(sys.argv[1], sys.argv[2])
+            crawler.run()
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    main()
