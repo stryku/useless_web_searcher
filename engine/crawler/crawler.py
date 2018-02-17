e3bc431606405dd4189eb4306df1b6f263af3101
stryku <stryku2393@gmail.com>
2018-02-17 22:05:14 +0100

    simplified Crawler.run method

--- engine/crawler/crawler.py
+++ engine/crawler/crawler.py
@@ -12,20 +12,30 @@ class Crawler:
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REQ)
 
+    def get_next_url_data(self):
+        print("sending request")
+        self.socket.send(b'')
+        received = self.socket.recv_string()
+        print("received response: %s" % received)
+        return json.loads(received)
+
+    def download_site_content(self, url):
+        print("downloading site from: %s" % url)
+        site = urllib.request.urlopen(url)
+        return site.read()
+
+    def store_site(self, url_data, site_content):
+        pass
+
     def run(self):
         print("connecting to crawl frontier: %s" % self.crawl_frontier_ip)
         self.socket.connect(self.crawl_frontier_ip)
 
         while True:
-            print("sending request")
-            self.socket.send(b'')
-            received = self.socket.recv_string()
-            print("received response: %s" % received)
-            data = json.loads(received)
-            print("downloading site from: %s" % data['url'])
-            site = urllib.request.urlopen(data['url'])
-            print(site.headers)
-            time.sleep(2)
+            url_data = self.get_next_url_data()
+            site_content = self.download_site_content(url_data['url'])
+            self.store_site(url_data, site_content)
+            time.sleep(4)
 
 
 def main():
