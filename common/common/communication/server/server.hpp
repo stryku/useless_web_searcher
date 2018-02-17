4ca72f1ee4e7714bced6e8f86fdbbdc8570c1b78
stryku <stryku2393@gmail.com>
2018-02-17 23:52:47 +0100

    added bind() method

--- common/common/communication/server/server.hpp
+++ common/common/communication/server/server.hpp
@@ -1,7 +1,21 @@
 #pragma once
 
+#include <zmq.hpp>
+
+#include <string>
+
 namespace usl::common::communication::server
 {
     class server
-    {};
+    {
+    public:
+        server(zmq::context_t& context)
+          : m_socket{ context, ZMQ_REP }
+        {}
+
+        void bind(std::string_view bind_address);
+
+    private:
+        zmq::socket_t m_socket;
+    };
 }
