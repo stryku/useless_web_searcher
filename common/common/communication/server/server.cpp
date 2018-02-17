dc5d87ba5328747bebef2e38e524a7ac99dadd64
stryku <stryku2393@gmail.com>
2018-02-17 23:54:38 +0100

    moved server methods to cpp

--- common/common/communication/server/server.cpp
+++ common/common/communication/server/server.cpp
@@ -1,3 +1,14 @@
 #include "common/communication/server/server.hpp"
 
+namespace usl::common::communication::server
+{
+    server::server(zmq::context_t& context)
+        : m_socket{ context, ZMQ_REP }
+    {}
+
+    void server::bind(std::string_view bind_address)
+    {
+        m_socket.bind(bind_address.c_str());
+    }
+}
 
--- common/common/communication/server/server.hpp
+++ common/common/communication/server/server.hpp
@@ -9,9 +9,7 @@ namespace usl::common::communication::server
     class server
     {
     public:
-        server(zmq::context_t& context)
-          : m_socket{ context, ZMQ_REP }
-        {}
+        server(zmq::context_t& context);
 
         void bind(std::string_view bind_address);
 
