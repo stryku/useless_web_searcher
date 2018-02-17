98605094ac86ec2fc96f9797ebc0d9601a8f85e6
stryku <stryku2393@gmail.com>
2018-02-17 21:46:05 +0100

    introduce fake crawl frontier

---
+++ engine/craw_frontier/fake_crawl_frontier.py
@@ -0,0 +1,31 @@
+import zmq
+import json
+
+
+def main():
+    print("Starting fake crawl frontier")
+
+    fake_id = 0
+    while True:
+        try:
+            context = zmq.Context()
+            socket = context.socket(zmq.REP)
+            socket.bind("tcp://*:5555")
+            message = socket.recv()
+            print("Received request: %s" % message)
+
+            data = {
+                'url': 'ACME',
+                'id': fake_id
+            }
+
+            json_str = json.dumps(data)
+            print("Sending response: %s" % json_str)
+            socket.send(json_str)
+            fake_id += 1
+        except Exception as ex:
+            print(ex)
+
+
+if __name__ == "__main__":
+    main()
