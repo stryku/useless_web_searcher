5330881510d7fa8708ab6a08cff8aa1b522f6916
stryku <stryku2393@gmail.com>
2018-02-18 00:50:24 +0100

    creating server in parse_frontier's main

--- engine/parse_frontier/main.cpp
+++ engine/parse_frontier/main.cpp
@@ -1,4 +1,20 @@
-int main()
+#include "common/communication/server/server_factory.hpp"
+#include "common/communication/server/server.hpp"
+
+#include <iostream>
+
+int main(int argc, char* argv[])
 {
+    if(argc < 3)
+    {
+        std::cout << "usage: parse_frontier working_directory_path bind_ip";
+        return 1;
+    }
+
+
+    const auto working_directory = argv[1];
+    const auto bind_address = argv[2];
+    auto server = usl::common::communication::server::server_factory{}.create(bind_address);
+
     return 0;
 }
