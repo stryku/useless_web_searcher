4e9527d77f8d79859599caa147489cd3d066b8a2
stryku <stryku2393@gmail.com>
2018-02-17 23:51:52 +0100

    introduce communication context

---
+++ common/common/communication/context.cpp
@@ -0,0 +1,6 @@
+#include "common/communication/context.hpp"
+
+namespace usl::common::communication
+{
+    zmq::context_t context;
+}
---
+++ common/common/communication/context.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <zmq.hpp>
+
+namespace usl::common::communication
+{
+    extern zmq::context_t context;
+}
