0386ff0108449ea1da13097230a9581bb66e0e81
stryku <stryku2393@gmail.com>
2018-02-17 21:59:55 +0100

    connection to crawl frontier works

--- engine/crawler/crawler.py
+++ engine/crawler/crawler.py
@@ -1,16 +1,31 @@
 import urllib.request
 import sys
+import json
+import zmq
+import time
 
 
 class Crawler:
     def __init__(self, working_dir, crawl_frontier_ip):
         self.working_dir = working_dir
         self.crawl_frontier_ip = crawl_frontier_ip
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REQ)
 
     def run(self):
+        print("connecting to crawl frontier: %s" % self.crawl_frontier_ip)
+        self.socket.connect(self.crawl_frontier_ip)
+
         while True:
-            site = urllib.request.urlopen("https://stackoverflow.com")
+            print("sending request")
+            self.socket.send(b'')
+            received = self.socket.recv_string()
+            print("received response: %s" % received)
+            data = json.loads(received)
+            print("downloading site from: %s" % data['url'])
+            site = urllib.request.urlopen(data['url'])
             print(site.headers)
+            time.sleep(2)
 
 
 def main():
