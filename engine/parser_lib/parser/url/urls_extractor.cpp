6035e2e7b5b1d7f3b15e8bd27d8daebe611148d5
stryku <stryku2393@gmail.com>
2018-02-18 04:29:18 +0100

    added urls_extracot::find_body

--- engine/parser_lib/parser/url/urls_extractor.cpp
+++ engine/parser_lib/parser/url/urls_extractor.cpp
@@ -1,5 +1,18 @@
-//
-// Created by stryku on 18.02.18.
-//
+#include "parser/url/urls_extractor.hpp"
 
-#include "urls_extractor.hpp"
+namespace usl::parser::url
+{
+    std::vector<string_view> urls_extractor::extract(string_view content) const
+    {
+        std::vector<string_view> urls;
+
+        auto pos = find_body(content);
+
+        return urls;
+    }
+
+    string_view::size_type urls_extractor::find_body(string_view content) const
+    {
+        return content.find("<body");
+    }
+}
--- engine/parser_lib/parser/url/urls_extractor.hpp
+++ engine/parser_lib/parser/url/urls_extractor.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
+#include "common/string_view.hpp"
+
+#include <vector>
+
 namespace usl::parser::url
 {
     class urls_extractor
-    {};
+    {
+    public:
+        std::vector<string_view> extract(string_view content) const;
+
+    private:
+        string_view::size_type find_body(string_view content) const;
+    };
 }
