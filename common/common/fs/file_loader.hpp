32021b35a932ae2d30081a586b4013accee42369
stryku <stryku2393@gmail.com>
2018-02-18 15:31:38 +0100

    added guard if file exists

--- common/common/fs/file_loader.hpp
+++ common/common/fs/file_loader.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <boost/filesystem.hpp>
+
 #include <vector>
 #include <cstdint>
 #include <string>
@@ -13,13 +15,18 @@ namespace usl::common::fs
         template <typename Buffer>
         Buffer load(const std::string& path) const
         {
-            std::ifstream t(path, std::ios::binary);
-            t.seekg(0, std::ios::end);
-            const auto size = t.tellg();
             Buffer buffer;
-            buffer.resize(size);
-            t.seekg(0);
-            t.read(reinterpret_cast<char*>(&buffer[0]), size);
+
+            if(boost::filesystem::exists(path))
+            {
+                std::ifstream t(path, std::ios::binary);
+                t.seekg(0, std::ios::end);
+                const auto size = t.tellg();
+                buffer.resize(size);
+                t.seekg(0);
+                t.read(reinterpret_cast<char*>(&buffer[0]), size);
+            }
+
             return buffer;
         }
     };
