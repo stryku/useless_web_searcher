496208e667c3ff0441c7f56dc0ae0597e43a4f25
stryku <stryku2393@gmail.com>
2018-02-18 02:23:56 +0100

    introduce content_handler

---
+++ engine/parser_lib/parser/content/details/content_handler.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace usl::parser::content::details
+{
+    class base_content_handler
+    {
+    public:
+        virtual void handle(const std::string& content) = 0;
+    };
+
+    template <typename T>
+    class content_handler_impl : public base_content_handler
+    {
+    public:
+        content_handler_impl(T content_handler)
+            : m_content_handler{ std::move(content_handler) }
+        {}
+
+        void handle(const std::string& content) override
+        {
+            m_content_handler.handle(content);
+        }
+
+    private:
+        T m_content_handler;
+    };
+}
