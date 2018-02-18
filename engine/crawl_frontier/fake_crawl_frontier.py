74981a6ab6feb15d290980ed11b44970d2cc2d88
stryku <stryku2393@gmail.com>
2018-02-18 19:29:04 +0100

    fake_crawl_frontier gets urls from db

--- engine/crawl_frontier/fake_crawl_frontier.py
+++ engine/crawl_frontier/fake_crawl_frontier.py
@@ -2,6 +2,7 @@ import zmq
 import json
 
 
+
 def main():
     print("Starting fake crawl frontier")
 
@@ -10,14 +11,30 @@ def main():
         try:
             context = zmq.Context()
             socket = context.socket(zmq.REP)
+            db_address = 'tcp://localhost:5553'
             socket.bind("tcp://*:5555")
             message = socket.recv()
             print("Received request: %s" % message)
 
-            data = {
+            db_socket = context.socket(zmq.REQ)
+            db_socket.connect(db_address)
+
+            db_req_data = {
+                'type': 'get_for_processing'
+            }
+            db_json_str = json.dumps(db_req_data)
+            db_socket.send_string(db_json_str)
+            db_msg = db_socket.recv_string()
+            print("received from db: %s" % db_msg)
+
+            data = json.load_string(db_msg)
+
+
+            """data = {
                 'url': 'http://en.cppreference.com/w/cpp/string/basic_string_view/find',
                 'id': fake_id
-            }
+            }"""
+
 
             json_str = json.dumps(data)
             print("Sending response: %s" % json_str)
