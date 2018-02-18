241295e3987817b030eab7f5c33f45eb9aa75009
stryku <stryku2393@gmail.com>
2018-02-18 01:11:23 +0100

    added logs to parse frontier main

--- engine/parse_frontier/main.cpp
+++ engine/parse_frontier/main.cpp
@@ -16,6 +16,11 @@ int main(int argc, char* argv[])
 
     const auto working_directory = argv[1];
     const auto bind_address = argv[2];
+
+    LOG(INFO) << "Parse frontier starting.";
+    LOG(INFO) << "Working directory: " << working_directory;
+    LOG(INFO) << "Bind address: " << bind_address;
+
     auto server = usl::common::communication::server::server_factory{}.create(bind_address);
 
     server.set_message_handler(usl::parse_frontier::request_message_handler{});
