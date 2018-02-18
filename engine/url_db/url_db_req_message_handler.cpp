eeb15f43b9cf81fdc8b24be0a538403446cb8ae4
stryku <stryku2393@gmail.com>
2018-02-18 16:18:42 +0100

    implementing handling insert request

--- engine/url_db/url_db_req_message_handler.cpp
+++ engine/url_db/url_db_req_message_handler.cpp
@@ -1,6 +1,7 @@
 #include "url_db_req_message_handler.hpp"
 
-#include <boost/property_tree/ptree.hpp>
+#include "url_db/url_db.hpp"
+
 #include <boost/property_tree/json_parser.hpp>
 
 #include <easylogging/easylogging++.h>
@@ -9,6 +10,9 @@
 
 namespace usl::url_db
 {
+    url_db_req_message_handler::url_db_req_message_handler(url_db& db)
+        : m_db{ db }
+    {}
 
     zmq::message_t url_db_req_message_handler::handle(const zmq::message_t& req)
     {
@@ -21,7 +25,9 @@ namespace usl::url_db
 
         pt::read_json(iss, tree);
 
-        if(tree.get<std::string>("type") == "insert")
+        const auto msg_type = tree.get<std::string>("type");
+
+        if(msg_type == "insert")
         {
             return insert(tree.get<std::string>("url"));
         }
@@ -29,18 +35,24 @@ namespace usl::url_db
         return zmq::message_t();
     }
 
-    zmq::message_t url_db_req_message_handler::get_for_processing(const zmq::message_t& req)
+    zmq::message_t url_db_req_message_handler::get_for_processing(boost::property_tree::ptree& parsed_req)
     {
         return zmq::message_t();
     }
 
-    zmq::message_t url_db_req_message_handler::get(const zmq::message_t& req)
+    zmq::message_t url_db_req_message_handler::get(boost::property_tree::ptree& parsed_req)
     {
         return zmq::message_t();
     }
 
-    zmq::message_t url_db_req_message_handler::insert(const std::string& req)
+    zmq::message_t url_db_req_message_handler::insert(boost::property_tree::ptree& parsed_req)
     {
-        return zmq::message_t();
+        const auto url = parsed_req.get<std::string>("url");
+
+        m_db.insert(url);
+
+        const auto ok = "ok";
+
+        return zmq::message_t{ std::begin(ok), std::end(ok) };
     }
 }
--- engine/url_db/url_db_req_message_handler.hpp
+++ engine/url_db/url_db_req_message_handler.hpp
@@ -1,17 +1,25 @@
 #pragma once
 
+#include <boost/property_tree/ptree.hpp>
+
 #include <zmq.hpp>
 
 namespace usl::url_db
 {
+    class url_db;
+
     class url_db_req_message_handler
     {
     public:
+        explicit url_db_req_message_handler(url_db& db);
+
         zmq::message_t handle(const zmq::message_t& req);
 
     private:
-        zmq::message_t get_for_processing(const zmq::message_t& req);
-        zmq::message_t get(const zmq::message_t& req);
-        zmq::message_t insert(const std::string& req);
+        zmq::message_t get_for_processing(boost::property_tree::ptree& parsed_req);
+        zmq::message_t get(boost::property_tree::ptree& parsed_req);
+        zmq::message_t insert(boost::property_tree::ptree& parsed_req);
+
+        url_db& m_db;
     };
 }
