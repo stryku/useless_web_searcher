5f03d4760962a3e6b5a70e59014f9357794214eb
stryku <stryku2393@gmail.com>
2018-02-24 14:46:50 +0100

    added find_next_text() method

--- engine/parser_lib/parser/content/text_content_extractor.cpp
+++ engine/parser_lib/parser/content/text_content_extractor.cpp
@@ -14,4 +14,15 @@ namespace usl::parser::content
         const auto end = site_content.find("</body", begin);
         return { begin, end };
     }
+
+    std::string text_content_extractor::extract_text(const text_content_extractor::pos_bounds_t& body_bounds) const
+    {
+    }
+
+    text_content_extractor::pos_bounds_t text_content_extractor::find_next_text(const std::string &content,
+                                                                                text_content_extractor::pos_t from) const
+    {
+        const auto next_tag_begin = content.find('<', from);
+        return { from, next_tag_begin };
+    }
 }
--- engine/parser_lib/parser/content/text_content_extractor.hpp
+++ engine/parser_lib/parser/content/text_content_extractor.hpp
@@ -14,5 +14,7 @@ namespace usl::parser::content
         using pos_bounds_t = std::pair<pos_t, pos_t>;
 
         pos_bounds_t find_body_pos(const std::string& site_content) const;
+        std::string extract_text(const pos_bounds_t& body_bounds) const;
+        pos_bounds_t find_next_text(const std::string& content, pos_t from) const;
     };
 }
