13037183004139948ea0c5ed7d4f2fbc79c10e4f
stryku <stryku2393@gmail.com>
2018-02-24 14:49:41 +0100

    text_content_parser accepts site_content in the ctor

--- engine/parser_lib/parser/content/text_content_extractor.cpp
+++ engine/parser_lib/parser/content/text_content_extractor.cpp
@@ -2,27 +2,38 @@
 
 namespace usl::parser::content
 {
-    std::string text_content_extractor::extract(const std::string &site_content) const
+    text_content_extractor::text_content_extractor(const std::string &site_content)
+        : m_site_content{ site_content }
+    {}
+
+    std::string text_content_extractor::extract() const
     {
 
         return std::__cxx11::string();
     }
 
-    text_content_extractor::pos_bounds_t text_content_extractor::find_body_pos(const std::string& site_content) const
+    text_content_extractor::pos_bounds_t text_content_extractor::find_body_pos() const
     {
-        const auto begin = site_content.find("<body");
-        const auto end = site_content.find("</body", begin);
+        const auto begin = m_site_content.find("<body");
+        const auto end = m_site_content.rfind("</body", begin);
         return { begin, end };
     }
 
     std::string text_content_extractor::extract_text(const text_content_extractor::pos_bounds_t& body_bounds) const
     {
+        auto tag_begin = body_bounds.first;
+
     }
 
-    text_content_extractor::pos_bounds_t text_content_extractor::find_next_text(const std::string &content,
-                                                                                text_content_extractor::pos_t from) const
+    text_content_extractor::pos_bounds_t text_content_extractor::find_next_text(text_content_extractor::pos_t from) const
     {
-        const auto next_tag_begin = content.find('<', from);
+        const auto next_tag_begin = m_site_content.find('<', from);
         return { from, next_tag_begin };
+    }
+
+    string_view text_content_extractor::get_next_tag(text_content_extractor::pos_t from) const
+    {
+
+        return usl::string_view();
     }
 }
--- engine/parser_lib/parser/content/text_content_extractor.hpp
+++ engine/parser_lib/parser/content/text_content_extractor.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "common/string_view.hpp"
+
 #include <string>
 
 namespace usl::parser::content
@@ -7,14 +9,20 @@ namespace usl::parser::content
     class text_content_extractor
     {
     public:
-        std::string extract(const std::string& site_content) const;
+        text_content_extractor(const std::string& site_content);
+
+        std::string extract() const;
 
     private:
         using pos_t = std::string::size_type;
         using pos_bounds_t = std::pair<pos_t, pos_t>;
 
-        pos_bounds_t find_body_pos(const std::string& site_content) const;
+        pos_bounds_t find_body_pos() const;
         std::string extract_text(const pos_bounds_t& body_bounds) const;
-        pos_bounds_t find_next_text(const std::string& content, pos_t from) const;
+        pos_bounds_t find_next_text(pos_t from) const;
+        string_view get_next_tag(pos_t from) const;
+
+    private:
+        const std::string& m_site_content;
     };
 }
