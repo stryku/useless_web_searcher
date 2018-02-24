20a02bd2bc664a6029cf67970d72ff41d6477354
stryku <stryku2393@gmail.com>
2018-02-24 23:33:52 +0100

    make sure that page rank will be >= 1.0

--- engine/index_lib/index/page_rank/page_rank.cpp
+++ engine/index_lib/index/page_rank/page_rank.cpp
@@ -8,9 +8,10 @@ namespace usl::index::page_rank
 
     void page_rank::new_page_reference(common::db::url_id_t id)
     {
-        const auto k_page_reference_points = 0.1;
+        const auto k_page_reference_points = .1;
         const auto current_rank = m_page_rank_storage.get_rank(id);
-        m_page_rank_storage.update(id, current_rank + k_page_reference_points);
+        const auto fixed_rank = current_rank < 1. ? 1. : current_rank;
+        m_page_rank_storage.update(id, fixed_rank + k_page_reference_points);
     }
 
     double page_rank::get_rank(common::db::url_id_t id) const
