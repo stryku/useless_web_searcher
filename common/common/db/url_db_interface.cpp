09b3de2f195af8dcb0c643c920d5e9fcedb1fd6e
stryku <stryku2393@gmail.com>
2018-02-24 09:13:55 +0100

    simplified code - added send_and_recv() method

--- common/common/db/url_db_interface.cpp
+++ common/common/db/url_db_interface.cpp
@@ -11,60 +11,28 @@ namespace usl::common::db
 
     std::string url_db_interface::insert(const std::string &url)
     {
-        LOG(INFO) << "url_db_interface insert: " << url;
-
-        boost::property_tree::ptree tree;
-        tree.put("type", "insert");
-        tree.put("url", url);
-
-        std::ostringstream oss;
-        boost::property_tree::write_json(oss, tree);
-
-        const auto str_msg = oss.str();
-
-        zmq::message_t msg{ str_msg.begin(), str_msg.end() };
-
-        m_socket.send(msg);
-
-        zmq::message_t reply;
-        m_socket.recv (&reply);
-
-        const auto str_reply = std::string{ reply.data<char>(), reply.size() };
-
-        return str_reply;
+        return send_and_recv("insert", "url", url);
     }
 
     std::string url_db_interface::get(common::db::url_id_t id)
     {
-        LOG(INFO) << "url_db_interface get: " << id;
-
-        boost::property_tree::ptree tree;
-        tree.put("type", "get");
-        tree.put("url", id);
-
-        std::ostringstream oss;
-        boost::property_tree::write_json(oss, tree);
-
-        const auto str_msg = oss.str();
-
-        zmq::message_t msg{ str_msg.begin(), str_msg.end() };
-        m_socket.send(msg);
-
-        zmq::message_t reply;
-        m_socket.recv (&reply);
-
-        const auto str_reply = std::string{ reply.data<char>(), reply.size() };
-
-        return str_reply;
+        return send_and_recv("get", "id", id);
     }
 
     std::string url_db_interface::update_state_as_processed(common::db::url_id_t id)
     {
-        LOG(INFO) << "url_db_interface update_state_as_processed: " << id;
+        return send_and_recv("update_state_as_processed", "id", id);
+    }
+
+    std::string url_db_interface::send_and_recv(const std::string &type,
+                                                const std::string &field,
+                                                const std::string &field_value)
+    {
+        LOG(INFO) << "url_db_interface " << type << " " << field_value;
 
         boost::property_tree::ptree tree;
         tree.put("type", "update_state_as_processed");
-        tree.put("url", id);
+        tree.put(field, field_value);
 
         std::ostringstream oss;
         boost::property_tree::write_json(oss, tree);
@@ -77,8 +45,6 @@ namespace usl::common::db
         zmq::message_t reply;
         m_socket.recv (&reply);
 
-        const auto str_reply = std::string{ reply.data<char>(), reply.size() };
-
-        return str_reply;
+        return std::string{ reply.data<char>(), reply.size() };
     }
 }
--- common/common/db/url_db_interface.hpp
+++ common/common/db/url_db_interface.hpp
@@ -25,6 +25,15 @@ namespace usl::common::db
         std::string update_state_as_processed(common::db::url_id_t id);
 
     private:
+        template <typename T, typename = std::enable_if_t<!std::is_same<T, std::string>::value>>
+        std::string send_and_recv(const std::string& type, const std::string& field, const T& field_value)
+        {
+            return send_and_recv(type, field, std::to_string(field_value));
+        }
+
+        std::string send_and_recv(const std::string& type, const std::string& field, const std::string& field_value);
+
+    private:
         zmq::socket_t m_socket;
     };
 }
