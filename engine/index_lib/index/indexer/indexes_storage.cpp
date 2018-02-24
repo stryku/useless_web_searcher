e37b7588c7bc62dd732407906a8bf0ced5de7f62
stryku <stryku2393@gmail.com>
2018-02-24 20:25:07 +0100

    added ctor()

--- engine/index_lib/index/indexer/indexes_storage.cpp
+++ engine/index_lib/index/indexer/indexes_storage.cpp
@@ -1,5 +1,12 @@
 #include "index/indexer/indexes_storage.hpp"
 
+#include <boost/filesystem.hpp>
+
 namespace usl::index::indexer
 {
+    indexes_storage::indexes_storage(const std::string &working_directory)
+        : m_indexes_dir{ working_directory + "/index/indexes" }
+    {
+        boost::filesystem::create_directories(m_indexes_dir);
+    }
 }
--- engine/index_lib/index/indexer/indexes_storage.hpp
+++ engine/index_lib/index/indexer/indexes_storage.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
+#include <string>
+
 namespace usl::index::indexer
 {
     class indexes_storage
     {
+    public:
+        explicit indexes_storage(const std::string& working_directory);
 
+    private:
+        const std::string m_indexes_dir;
     };
 }
