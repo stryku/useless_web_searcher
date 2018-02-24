c1e69892dbe2dc2c01b9dfd6e02dea0537e00119
stryku <stryku2393@gmail.com>
2018-02-24 20:05:35 +0100

    implemented new_page_reference()

--- engine/index_lib/index/page_rank/page_rank.cpp
+++ engine/index_lib/index/page_rank/page_rank.cpp
@@ -2,9 +2,14 @@
 
 namespace usl::index::page_rank
 {
+    page_rank::page_rank(const std::string &working_directory)
+        : m_page_rank_storage{ working_directory }
+    {}
 
     void page_rank::new_page_reference(common::db::url_id_t id)
     {
-
+        const auto k_page_reference_points = 0.1;
+        const auto current_rank = m_page_rank_storage.get_rank(id);
+        m_page_rank_storage.update(id, current_rank + k_page_reference_points);
     }
 }
--- engine/index_lib/index/page_rank/page_rank.hpp
+++ engine/index_lib/index/page_rank/page_rank.hpp
@@ -2,11 +2,18 @@
 
 #include "common/db/url_id.hpp"
 
+#include "index/page_rank/page_rank_storage.hpp"
+
 namespace usl::index::page_rank
 {
     class page_rank
     {
     public:
+        explicit page_rank(const std::string& working_directory);
+
         void new_page_reference(common::db::url_id_t id);
+
+    private:
+        page_rank_storage m_page_rank_storage;
     };
 }
