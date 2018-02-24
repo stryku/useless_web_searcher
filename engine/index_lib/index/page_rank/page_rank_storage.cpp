75f84cdb3510f6c834e411134d534b40aa35a17b
stryku <stryku2393@gmail.com>
2018-02-24 20:00:05 +0100

    added is_in_file()

--- engine/index_lib/index/page_rank/page_rank_storage.cpp
+++ engine/index_lib/index/page_rank/page_rank_storage.cpp
@@ -13,7 +13,7 @@ namespace usl::index::page_rank
         common::fs::file_creator{ m_storage_file_path };
     }
 
-    void page_rank_storage::update(common::db::url_id_t id, double rank)
+    void page_rank_storage::update(common::db::url_id_t id, double rank) const
     {
         if(need_grow(id))
         {
@@ -28,8 +28,7 @@ namespace usl::index::page_rank
 
     bool page_rank_storage::need_grow(common::db::url_id_t id) const
     {
-        const auto file_size = boost::filesystem::file_size(m_storage_file_path);
-        return file_size < get_entry_offset(id);
+        return !is_in_file(id);
     }
 
     page_rank_storage::offset_t page_rank_storage::get_entry_offset(common::db::url_id_t id) const
@@ -48,5 +47,16 @@ namespace usl::index::page_rank
     {
         auto entry_ptr = std::next(file.data(), get_entry_offset(id));
         return *reinterpret_cast<page_rank_storage_entry*>(entry_ptr);
+    }
+
+    double page_rank_storage::get_rank(common::db::url_id_t id) const
+    {
+        return 0;
+    }
+
+    bool page_rank_storage::is_in_file(common::db::url_id_t id) const
+    {
+        const auto file_size = boost::filesystem::file_size(m_storage_file_path);
+        return file_size > get_entry_offset(id);
     }
 }
--- engine/index_lib/index/page_rank/page_rank_storage.hpp
+++ engine/index_lib/index/page_rank/page_rank_storage.hpp
@@ -15,12 +15,14 @@ namespace usl::index::page_rank
     public:
         explicit page_rank_storage(const std::string& working_directory_path);
 
-        void update(common::db::url_id_t id, double rank);
+        void update(common::db::url_id_t id, double rank) const;
+        double get_rank(common::db::url_id_t id) const;
 
     private:
         using offset_t = uint64_t;
 
         bool need_grow(common::db::url_id_t id) const;
+        bool is_in_file(common::db::url_id_t id) const;
         offset_t get_entry_offset(common::db::url_id_t id) const;
 
         void grow(common::db::url_id_t id) const;
