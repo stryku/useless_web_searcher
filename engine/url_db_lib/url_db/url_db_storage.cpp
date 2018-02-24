1bdd1af04f44a76cd0539cec9859548ed3f7aa5d
stryku <stryku2393@gmail.com>
2018-02-24 08:24:15 +0100

    use common::db::url_state instead of raw uint8_t

--- engine/url_db_lib/url_db/url_db_storage.cpp
+++ engine/url_db_lib/url_db/url_db_storage.cpp
@@ -1,6 +1,8 @@
 #include "url_db/url_db_storage.hpp"
 
+#include "common/db/url_state.hpp"
 #include "common/fs/file_loader.hpp"
+
 
 namespace usl::url_db
 {
@@ -28,15 +30,17 @@ namespace usl::url_db
     {
         std::ofstream file{ m_data_file_path, std::ios::out | std::ios::app | std::ios::binary };
 
-        uint8_t state{ 0u };
-        file.write(reinterpret_cast<char*>(&state), sizeof(state));
-        file.write(url.data(), url.size() + 1u);
+        const auto url_size_with_null = url.size() + 1u;
+
+        const auto kState = common::db::url_state::not_processed;
+        file.write(reinterpret_cast<const char*>(&kState), sizeof(kState));
+        file.write(url.data(), url_size_with_null);
 
         const auto insert_offset = m_data.size();
 
-        m_data.push_back(state);
+        m_data.push_back(static_cast<uint8_t>(kState));
         const auto url_begin = reinterpret_cast<const uint8_t*>(url.data());
-        const auto url_end = std::next(url_begin, url.size() + 1u); //with null
+        const auto url_end = std::next(url_begin, url_size_with_null);
         m_data.insert(m_data.end(), url_begin, url_end);
 
         return insert_offset;
