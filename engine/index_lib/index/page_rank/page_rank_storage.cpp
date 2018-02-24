e14624694a4b1a4026ba1459ba54ca3a0b47c260
stryku <stryku2393@gmail.com>
2018-02-24 19:45:34 +0100

    page_rank_storage creates page_rank file in ctor

--- engine/index_lib/index/page_rank/page_rank_storage.cpp
+++ engine/index_lib/index/page_rank/page_rank_storage.cpp
@@ -1,9 +1,25 @@
 #include "page_rank_storage.hpp"
 
+#include "common/fs/file_creator.hpp"
+
+#include <boost/filesystem.hpp>
+
 namespace usl::index::page_rank
 {
+    page_rank_storage::page_rank_storage(const std::string &working_directory_path)
+        : m_storage_file_path{ working_directory_path + "/index/page_rank" }
+    {
+        common::fs::file_creator{ m_storage_file_path };
+    }
+
     void page_rank_storage::update(common::db::url_id_t id, double rank)
     {
 
+    }
+
+    bool page_rank_storage::need_grow(common::db::url_id_t id) const
+    {
+
+        return false;
     }
 }
--- engine/index_lib/index/page_rank/page_rank_storage.hpp
+++ engine/index_lib/index/page_rank/page_rank_storage.hpp
@@ -2,6 +2,8 @@
 
 #include "common/db/url_id.hpp"
 
+#include <string>
+
 namespace usl::index::page_rank
 {
     struct page_rank_entry;
@@ -9,6 +11,14 @@ namespace usl::index::page_rank
     class page_rank_storage
     {
     public:
+        explicit page_rank_storage(const std::string& working_directory_path);
+
         void update(common::db::url_id_t id, double rank);
+
+    private:
+        bool need_grow(common::db::url_id_t id) const;
+
+    private:
+        const std::string m_storage_file_path;
     };
 }
