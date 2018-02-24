1187478caaa29c63ceed669f7702f1c33a43a42a
stryku <stryku2393@gmail.com>
2018-02-24 20:54:34 +0100

    added new_hit()

--- engine/index_lib/index/indexer/index_file.cpp
+++ engine/index_lib/index/indexer/index_file.cpp
@@ -1,5 +1,17 @@
 #include "index/indexer/index_file.hpp"
 
+#include "common/fs/file_creator.hpp"
+
 namespace usl::index::indexer
 {
+    index_file::index_file(const std::string &path)
+        : m_path{ path }
+    {
+        common::fs::file_creator{ m_path };
+    }
+
+    void index_file::new_hit(common::db::url_id_t id) const
+    {
+
+    }
 }
--- engine/index_lib/index/indexer/index_file.hpp
+++ engine/index_lib/index/indexer/index_file.hpp
@@ -1,9 +1,19 @@
 #pragma once
 
+#include "common/db/url_id.hpp"
+
+#include <string>
+
 namespace usl::index::indexer
 {
     class index_file
     {
+    public:
+        explicit index_file(const std::string& path);
 
+        void new_hit(common::db::url_id_t id) const;
+
+    private:
+        const std::string m_path;
     };
 }
