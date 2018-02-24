bef19c73e08a7369952f0538b1fc1d18f23ff030
stryku <stryku2393@gmail.com>
2018-02-24 18:41:42 +0100

    introduce index request keys

---
+++ common/common/index/request_keys.cpp
@@ -0,0 +1,6 @@
+#include "common/index/request_keys.hpp"
+
+namespace usl::common::index::request_keys
+{
+    const std::string k_process_sentences = "process_sentences";
+}
---
+++ common/common/index/request_keys.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+namespace usl::common::index::request_keys
+{
+    extern const std::string k_process_sentences;
+}
