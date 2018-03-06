c606c746c76325f4e19e00f3c4511e26186896c9
stryku <stryku2393@gmail.com>
2018-03-06 20:31:39 +0100

    introduce get_urls_collection() and get_urls_filter() utils

--- engine/parser_lib/tests/url_filter_tests.cpp
+++ engine/parser_lib/tests/url_filter_tests.cpp
@@ -2,11 +2,21 @@
 
 #include <gmock/gmock.h>
 
+template <typename... Args>
+auto get_urls_collection(Args&&... args)
+{
+    return usl::parser::url::urls_collection_t{ std::string(args)... };
+}
+
+auto get_urls_filter()
+{
+    return usl::parser::url::urls_filter{};
+}
+
 TEST(url_filter, Empty_get_Empty)
 {
-    usl::parser::url::urls_collection_t urls = { { "" } };
-
-    usl::parser::url::urls_filter filter;
+    const auto urls = get_urls_collection("");
+    auto filter =  get_urls_filter();
     const auto result = filter.filter_urls("", "", urls);
 
     ASSERT_THAT(result.size(), 0u);
